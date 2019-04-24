d0ec3af39e554e4c9f278d1c6203ae77654a487f
nickjevershed <nick.evershed@gmail.com>
2019-04-24 12:35:57 +1000

    dump data to gzipped csv for external use

--- uploadJson.py
+++ uploadJson.py
@@ -6,6 +6,8 @@ import io
 import scraperwiki
 import time
 import simplejson as json
+import gzip
+import pandas as pd
 
 def uploadJson(test):
 	AWS_KEY = os.environ['AWS_KEY_ID']
@@ -13,6 +15,8 @@ def uploadJson(test):
 
 	queryString = "* from ads"
 	queryResult = scraperwiki.sqlite.select(queryString)
+
+	pd.DataFrame(queryResult).to_csv('fb-data.csv.gz',compression='gzip')
 
 	remove = ["html", "verified", "image_url", "vid_image_url", "image_id", "vid_image_id", "images_uploaded", "av_img_url", "av_img_id","start_date"]
 
@@ -24,9 +28,9 @@ def uploadJson(test):
 
 	with open('fb-data.json','w') as fileOut:
 			fileOut.write(results)
-
+			
 	if not test:		
-		print("Connecting to S3")
+		print("Uploading JSON to S3")
 		bucket = 'gdn-cdn'
 		session = boto3.Session(
 		aws_access_key_id=AWS_KEY,
@@ -36,4 +40,9 @@ def uploadJson(test):
 		key = "2019/04/fb-ad-data/fd-data.json"
 		object = s3.Object(bucket, key)
 		object.put(Body=results, CacheControl="max-age=300", ACL='public-read')
-		print("Uploaded json")
+		print("Done")
+
+		print("Uploading CSV to S3")
+		key2 = "2019/04/fb-ad-data/fb-data.csv.gz"
+		s3.meta.client.upload_file('fb-data.csv.gz', bucket, key2, ExtraArgs={"CacheControl":"max-age=300", 'ACL':'public-read'})
+		print("Done")
